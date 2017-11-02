8cfded1eb13e117dd47ef00331b20e5491679163
Prima Aulia Gusta <primAulia@gmail.com>
2017-11-02 16:46:37 +0800

    latest test on `Meal` and `DeliveryOrder`

--- spec/models/delivery_order_spec.rb
+++ spec/models/delivery_order_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe DeliveryOrder, :type => :model do
-  it "is not valid without a order_id" do
-    delivery_order = DeliveryOrder.new(order_id: nil)
-    expect(delivery_order).to_not be_valid
-  end
+describe DeliveryOrder, :type => :model do
+  subject { DeliveryOrder.new() }
 
-  it "is not valid without a serving_datetime" do
-    delivery_order = DeliveryOrder.new(serving_datetime: nil)
-    expect(delivery_order).to_not be_valid
+  describe ".new" do
+    it "is not valid without a order_id" do
+      delivery_order = DeliveryOrder.new(order_id: nil)
+      expect(delivery_order).to_not be_valid
+    end
+
+    it "is not valid without a serving_datetime" do
+      delivery_order = DeliveryOrder.new(serving_datetime: nil)
+      expect(delivery_order).to_not be_valid
+    end
   end
 end
--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Meal, :type => :model do
-  it "is not valid without a name" do
-    meal = Meal.new(name: nil)
-    expect(meal).to_not be_valid
-  end
+describe Meal, :type => :model do
+  describe ".new" do
+    it "is not valid without a name" do
+      meal = Meal.new(name: nil)
+      expect(meal).to_not be_valid
+    end
 
-  it "is not valid without a byline" do
-    meal = Meal.new(byline: nil)
-    expect(meal).to_not be_valid
+    it "is not valid without a byline" do
+      meal = Meal.new(byline: nil)
+      expect(meal).to_not be_valid
+    end    
   end
 end
